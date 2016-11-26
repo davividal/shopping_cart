33ad5c49c5c996a4b25b6bef23a6d111182bb7b2
Davi Koscianski Vidal <davividal@gmail.com>
2016-11-26 14:48:16 -0200

    Adicionando entidades

---
+++ src/AppBundle/Entity/Trade.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * Trade
+ */
+class Trade
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $user_id;
+
+    /**
+     * @var integer
+     */
+    private $stock_option_id;
+
+    /**
+     * @var integer
+     */
+    private $quantity;
+
+    /**
+     * @var string
+     */
+    private $paid;
+
+    /**
+     * @var \AppBundle\Entity\User
+     */
+    private $user;
+
+    /**
+     * @var \AppBundle\Entity\StockOption
+     */
+    private $stock_option;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return Trade
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set stockOptionId
+     *
+     * @param integer $stockOptionId
+     *
+     * @return Trade
+     */
+    public function setStockOptionId($stockOptionId)
+    {
+        $this->stock_option_id = $stockOptionId;
+
+        return $this;
+    }
+
+    /**
+     * Get stockOptionId
+     *
+     * @return integer
+     */
+    public function getStockOptionId()
+    {
+        return $this->stock_option_id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return Trade
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set paid
+     *
+     * @param string $paid
+     *
+     * @return Trade
+     */
+    public function setPaid($paid)
+    {
+        $this->paid = $paid;
+
+        return $this;
+    }
+
+    /**
+     * Get paid
+     *
+     * @return string
+     */
+    public function getPaid()
+    {
+        return $this->paid;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Trade
+     */
+    public function setUser(\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set stockOption
+     *
+     * @param \AppBundle\Entity\StockOption $stockOption
+     *
+     * @return Trade
+     */
+    public function setStockOption(\AppBundle\Entity\StockOption $stockOption = null)
+    {
+        $this->stock_option = $stockOption;
+
+        return $this;
+    }
+
+    /**
+     * Get stockOption
+     *
+     * @return \AppBundle\Entity\StockOption
+     */
+    public function getStockOption()
+    {
+        return $this->stock_option;
+    }
+}
+
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $login;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return User
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
